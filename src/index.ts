b7a02606ddd67aadb687560cc2403d1d1badb6c2
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-02 12:39:19 +0100

    Feat: Add pg as depenedeny and add db interaction in server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import Express from "express";
+const { Pool, Client } = require('pg');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -9,6 +10,27 @@ const app = Express();
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+const client = new Client({
+  user: 'user',
+  host: 'postgres',
+  database: 'db',
+  password: 'password',
+  port: 5432,
+});
+
+client.connect()
+  .then(() => {
+    console.log('Connected to postgres');
+  })
+  .catch((err: Error) => {
+    console.log('Error connecting to postgres', err);
+  });
+
+client.query('SELECT NOW()', (err: Error, res: any) => {
+  console.log(err, res);
+  client.end();
 });
 
 app.listen(PORT, HOST, () => {

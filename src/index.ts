6f0e09748490fa914155c47c754e7e57878bf41e
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-02 13:17:30 +0100

    Feat: Removed db from index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import Express from "express";
-const { Pool, Client } = require('pg');
+
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -12,26 +12,8 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-const client = new Client({
-  user: 'user',
-  host: 'postgres',
-  database: 'db',
-  password: 'password',
-  port: 5432,
-});
-
-client.connect()
-  .then(() => {
-    console.log('Connected to postgres');
-  })
-  .catch((err: Error) => {
-    console.log('Error connecting to postgres', err);
-  });
-
-client.query('SELECT NOW()', (err: Error, res: any) => {
-  console.log(err, res);
-  client.end();
-});
+console.log('Hello from Node.js v12.16.1!');
+console.log('Press Ctrl+C to quit.');
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);

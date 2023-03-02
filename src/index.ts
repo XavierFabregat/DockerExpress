7d5be497398c7772d4c78c3587f33aadafb6e84c
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-02 14:47:19 +0100

    Feat: Create bootsrtap function to fire the server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import Express from "express";
+import sequlize from "./Models";
 
 
 const PORT = 8080;
@@ -12,9 +13,20 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-console.log('Hello from Node.js v12.16.1!');
+console.log(`Hello from Node.js ${process.version}!`);
 console.log('Press Ctrl+C to quit.');
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+
+(async () => {
+  try {
+    await sequlize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequlize.sync();
+    console.log("All models were synchronized successfully.");
+    app.listen(PORT, HOST, () => {
+      console.log(`Running on http://${HOST}:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})()

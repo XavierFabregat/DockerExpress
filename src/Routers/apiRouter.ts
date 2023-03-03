ed52f9bec6a7364b96d56175c09b16f4f235f019
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-03 12:12:04 +0100

    Feat: Add route for post user

--- src/Routers/apiRouter.ts
+++ src/Routers/apiRouter.ts
@@ -4,5 +4,6 @@ import UserController from "../Controllers/user.controller";
 const apiRouter = Router();
 
 apiRouter.get("/users", UserController.getUsers);
+apiRouter.post("/users", UserController.postUser);
 
 export default apiRouter;
